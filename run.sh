011d0552b94294416ccfc8ae3de119d0ef410e6a
NCDev <55439243+xenfovn@users.noreply.github.com>
2022-09-15 09:20:31 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 for i in /etc/nginx/conf.d/default.conf /etc/nginx/stream.conf; do
-	envsubst "`env | awk -F = '{printf \" \\\\$%s\", $1}'`" < $i.template > $i
+	envsubst "`env | awk -F = '{printf \" \\\\$%s\", $1}'`" < $i > $i
 done
 
 echo Executing nginx...
